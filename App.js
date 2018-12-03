0409a23714d221ab83a810de496cfcf73f310fb8
Victor Aubin <aubin.victor@gmail.com>
2018-12-03 17:18:18 +0100

    working on camera

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import BadInstagramCloneApp from './components/Camera'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -24,6 +25,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+          <BadInstagramCloneApp></BadInstagramCloneApp>
       </View>
     );
   }
