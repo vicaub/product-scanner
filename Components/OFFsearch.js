8346fefd420a4e1fc9fa8131042bb0c0a2a68939
Benoît <benoit@MacBook-Pro-de-Benoit.local>
2018-12-17 15:58:42 +0100

    added some fields in parsing

--- Components/OFFsearch.js
+++ Components/OFFsearch.js
@@ -16,7 +16,13 @@ class Search extends React.Component {
 
     loadProduct(barcode) {
         getProductFromBarcode(barcode).then(data => {
-            this.setState({ ProductJson: JSON.parse(data.results).product_name_fr })
+
+            var json_parsed = JSON.parse(data.results);
+            this.setState({ ProductJson: {"product_name":json_parsed.product_name_fr,
+                                                "image_url":json_parsed.selected_images.front.small.fr,
+                                                "quantity":json_parsed.serving_quantity,
+                                                "packaging":json_parsed.packaging,
+                                                "brands":json_parsed.brands}})
         });
     }
 
