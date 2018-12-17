92a76ec3c3f360bd5b2a69c752ba964f6a9670a2
Benoît <benoit@MacBook-Pro-de-Benoit.local>
2018-12-17 15:01:52 +0100

    add search component

--- Components/OFFsearch.js
+++ Components/OFFsearch.js
@@ -2,11 +2,29 @@
 
 import { FlatList } from 'react-native'
 import { getProductFromBarcode } from '../API/OFFApi'
+import WaterBottleJson from '../Helpers/data'
+import React from "react";
+
 
 class Search extends React.Component {
 
-    _loadProduct(barcode) {
-        getProductFromBarcode(barcode).then(data => console.log(data));
+
+    constructor(props) {
+        super(props);
+        this.state = { ProductJson: {} }
     }
 
+    _loadProduct(barcode) {
+        getProductFromBarcode(barcode).then(data => {
+            this.setState({ ProductJson: data.results })
+        });
+    }
+
+    render(){
+        return(<FlatList
+            data={this.state.ProductJson}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({item}) => <Text>{item.key}</Text>}
+        />)
+    }
 }
