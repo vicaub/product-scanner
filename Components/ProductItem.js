97beb858919d7f198d541cdc80e6ff5e2376314a
Cecile Gontier <cecile.gontier@student.ecp.fr>
2019-02-07 16:57:49 +0100

    added proptypes

--- Components/ProductItem.js
+++ Components/ProductItem.js
@@ -32,6 +32,8 @@ ProductItem.propTypes = {
         imageUrl: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
         barCode: PropTypes.string.isRequired,
+        categories: PropTypes.objectOf(PropTypes.string).isRequired,
+        nbScans: PropTypes.number.isRequired,
     }).isRequired,
     cartCounter: PropTypes.number,
 };
