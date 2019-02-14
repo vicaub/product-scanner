24f1fedd197b14d2be5cd8efd09373abcb3b2996
Delphine Shi <shi.delphine@gmail.com>
2019-02-14 17:51:36 +0100

    Refactor productItem testing

---
+++ Helper/testData.js
@@ -0,0 +1,10 @@
+/* Used for Jest tests */
+
+export default testProduct = {
+    barCode: '3257980252050',
+    name: 'Oeufs bio nature x6',
+    categories: ["Produits d'élevages", "Œufs d'oiseau", "Œufs", "Œufs de poules élevées en plein air"],
+    scanDate: new Date(),
+    nbScans: 1,
+    imageUrl: 'https://static.openfoodfacts.org/images/products/325/798/025/2050/front_fr.7.400.jpg',
+}
--- __tests__/ProductItemTest.js
+++ __tests__/ProductItemTest.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ProductItem from '../Components/ProductItem';
 
 import renderer from 'react-test-renderer';
-import baskets from '../Helper/basketData';
+import product from '../Helper/testData';
 
 test('ProductItem renders correctly', () => {
-    const tree = renderer.create(<ProductItem product={baskets[0]["products"][0]}/>).toJSON();
+    const tree = renderer.create(<ProductItem product={product}/>).toJSON();
     expect(tree).toMatchSnapshot();
 });
